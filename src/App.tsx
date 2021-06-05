f3c4a961258d6c1e39ab048ae8ded4cd48b5f2c8
xiaolinzi7118 <1106147743@qq.com>
2021-06-05 16:17:14 +0800

    底部导航栏基本完成

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,70 @@
+import Nav from "components/Nav";
+import {
+  HashRouter as Router,
+  Switch,
+  Route,
+  Link,
+  Redirect
+} from "react-router-dom";
+import styled from "styled-components";
+
+const Wrapper = styled.div`
+  height:100vh;
+  display:flex;
+  flex-direction:column;
+`;
+const Main = styled.div`
+  flex-grow:1;
+  overflow:auto;
+`;
+
+
 
 function App() {
   return (
-    <div>
-      hi
-    </div>
+    <Router>
+      <div>
+        <Wrapper>
+          <Main>
+            <Switch>
+              <Route path="/tags">
+                <Tags />
+              </Route>
+              <Route path="/money">
+                <Money />
+              </Route>
+              <Route path="/statistics">
+                <Statistics />
+              </Route>
+              <Redirect exact from="/" to="money" />
+              <Route path="*">
+                <NoMatch />
+              </Route>
+            </Switch>
+          </Main>
+          <Nav />
+        </Wrapper>
+      </div>
+    </Router>
   );
 }
 
-export default App;
+function Tags() {
+  return <h2>标签页</h2>;
+}
+
+function Money() {
+  return <h2>记账页</h2>;
+}
+
+function Statistics() {
+  return <h2>统计页</h2>;
+}
+
+function NoMatch() {
+  return (
+    <div>页面不存在哦</div>
+  )
+}
+
+export default App
