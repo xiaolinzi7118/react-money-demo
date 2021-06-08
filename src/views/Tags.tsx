97fface935b739672077abfb8115e17e0e720ef2
xiaolinzi7118 <1106147743@qq.com>
2021-06-08 10:31:24 +0800

    tagList样式完成

--- src/views/Tags.tsx
+++ src/views/Tags.tsx
@@ -1,14 +1,55 @@
+import Icon from 'components/icons';
 import Layout from 'components/Layout';
+import styled from 'styled-components';
 import { useTags } from 'useTags';
+
+
+const TagList = styled.ol`
+  font-size: 16px; 
+  background:white;
+  > li{
+    //#e5e5e7
+    border-bottom: 1px solid #d5d5d9;
+    line-height: 20px;
+    padding: 12px 16px 12px 0;
+    margin-left: 16px;
+    display:flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+`;
+const Button = styled.button`
+  font-size: 18px; border: none; padding: 8px 12px;
+  background: #f60; border-radius: 4px;
+  color: white;
+`;
+const Center = styled.div`
+  display:flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+`;
+const Space = styled.div`
+  height: 16px;
+`
 
 function Tags() {
     const { tags, setTags } = useTags()
     return (
         <Layout>
             <ol>
-                {tags.map(tag =>
-                    <li key={tag}>{tag}</li>)}
+                <TagList>
+                    {tags.map(tag =>
+                        <li key={tag}>
+                            <span>{tag}</span>
+                            <Icon name="right" />
+                        </li>)}
+                </TagList>
             </ol>
+            <Center>
+                <Space />
+                <Button>新增标签</Button>
+            </Center>
         </Layout>
     );
 }
